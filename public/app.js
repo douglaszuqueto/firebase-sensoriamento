4e7429bb39221803fde5598ae24aca9c116daa82
douglaszuqueto <douglas.zuqueto@gmail.com>
2017-02-17 22:36:40 -0200

    project configured

--- public/app.js
+++ public/app.js
@@ -1,49 +1,47 @@
+(function () {
 
-(function(){
-    
   // Inicia o firebase Firebase
   var config = {
-    apiKey: "AIzaSyA9qTYW2_ruMJj3i-NVF1mGJPjSaNeko7k",
-    authDomain: "sensoriamento-iot-bootcamp.firebaseapp.com",
-    databaseURL: "https://sensoriamento-iot-bootcamp.firebaseio.com",
-    storageBucket: "sensoriamento-iot-bootcamp.appspot.com",
-    messagingSenderId: "1055433905603"
+    apiKey           : "AIzaSyBX3XR39UOcd4RpsXKSSHSPFRspoPPx_jA",
+    authDomain       : "douglaszuqueto-iotbr.firebaseapp.com",
+    databaseURL      : "https://douglaszuqueto-iotbr.firebaseio.com",
+    storageBucket    : "douglaszuqueto-iotbr.appspot.com",
+    messagingSenderId: "507017348605"
   };
   firebase.initializeApp(config);
 
   var db = firebase.database();
 
   // Cria os listeners dos dados no firebase
-  var tempRef = db.ref('temperature');
-  var umidRef = db.ref('humidity');
+  var tempRef     = db.ref('temperature');
+  var umidRef     = db.ref('humidity');
   var presenceRef = db.ref('presence');
-  var lampRef = db.ref('lamp');
-
+  var lampRef     = db.ref('lamp');
 
   // Registra as funções que atualizam os gráficos e dados atuais da telemetria
-  tempRef.on('value', onNewData('currentTemp', 'tempLineChart' , 'Temperatura', 'C°'));
-  umidRef.on('value', onNewData('currentUmid', 'umidLineChart' , 'Umidade', '%'));
+  tempRef.on('value', onNewData('currentTemp', 'tempLineChart', 'Temperatura', 'C°'));
+  umidRef.on('value', onNewData('currentUmid', 'umidLineChart', 'Umidade', '%'));
 
 
   // Registrar função ao alterar valor de presença
-  presenceRef.on('value', function(snapshot){
+  presenceRef.on('value', function (snapshot) {
     var value = snapshot.val();
-    var el = document.getElementById('currentPresence')
-    if(value){
+    var el    = document.getElementById('currentPresence');
+    if ( value ) {
       el.classList.add('green-text');
-    }else{
+    } else {
       el.classList.remove('green-text');
     }
   });
 
   // Registrar função ao alterar valor da lampada
   var currentLampValue = false;
-  lampRef.on('value', function(snapshot){
+  lampRef.on('value', function (snapshot) {
     var value = snapshot.val();
-    var el = document.getElementById('currentLamp')
-    if(value){
+    var el    = document.getElementById('currentLamp');
+    if ( value ) {
       el.classList.add('amber-text');
-    }else{
+    } else {
       el.classList.remove('amber-text');
     }
     currentLampValue = !!value;
@@ -51,7 +49,7 @@
 
   // Registrar função de click no botão de lampada
   var btnLamp = document.getElementById('btn-lamp');
-  btnLamp.addEventListener('click', function(evt){
+  btnLamp.addEventListener('click', function (evt) {
     lampRef.set(!currentLampValue);
   });
 
@@ -61,41 +59,41 @@
 // Retorna uma função que de acordo com as mudanças dos dados
 // Atualiza o valor atual do elemento, com a metrica passada (currentValueEl e metric)
 // e monta o gráfico com os dados e descrição do tipo de dados (chartEl, label)
-function onNewData(currentValueEl, chartEl, label, metric){
-  return function(snapshot){
+function onNewData(currentValueEl, chartEl, label, metric) {
+  return function (snapshot) {
     var readings = snapshot.val();
-    if(readings){
-        var currentValue;
-        var data = [];
-        for(var key in readings){
-          currentValue = readings[key]
-          data.push(currentValue);
-        }
+    if ( readings ) {
+      var currentValue;
+      var data = [];
+      for ( var key in readings ) {
+        currentValue = readings[key];
+        data.push(currentValue);
+      }
 
-        document.getElementById(currentValueEl).innerText = currentValue + ' ' + metric;
-        buildLineChart(chartEl, label, data);
+      document.getElementById(currentValueEl).innerText = currentValue + ' ' + metric;
+      buildLineChart(chartEl, label, data);
     }
   }
 }
 
 // Constroi um gráfico de linha no elemento (el) com a descrição (label) e os
 // dados passados (data)
-function buildLineChart(el, label, data){
+function buildLineChart(el, label, data) {
   var elNode = document.getElementById(el);
   new Chart(elNode, {
     type: 'line',
     data: {
-        labels: new Array(data.length).fill(""),
-        datasets: [{
-            label: label,
-            data: data,
-            borderWidth: 1,
-            fill: false,
-            spanGaps: false,
-            lineTension: 0.1,
-            backgroundColor: "#F9A825",
-            borderColor: "#F9A825"
-        }]
+      labels  : new Array(data.length).fill(""),
+      datasets: [{
+        label          : label,
+        data           : data,
+        borderWidth    : 1,
+        fill           : false,
+        spanGaps       : false,
+        lineTension    : 0.1,
+        backgroundColor: "#F9A825",
+        borderColor    : "#F9A825"
+      }]
     }
   });
 }
